bc49ebbfdf569cb8df1c05cdf88e6a2df2361f5e
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2019-03-30 21:54:26 +0530

    Add UI (beta)

---
+++ UI/js/upload.js
@@ -0,0 +1,45 @@
+var selectedFile;
+
+var config = {
+    apiKey: "AIzaSyAlkTeBTBPtnVAaOnNmwiwsFVDIWKhfp5M",
+    authDomain: "hackathon-mozofest-2019.firebaseapp.com",
+    databaseURL: "https://hackathon-mozofest-2019.firebaseio.com",
+    storageBucket: 'gs://hackathon-mozofest-2019.appspot.com/'
+};
+firebase.initializeApp(config);
+
+
+
+
+function uploadFile(){
+    var filename = selectedFile.name;
+    var storageRef = firebase.storage().ref('/testImages/'+ filename); 
+    var uploadTask = storageRef.put(selectedFile);
+    
+
+    uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log('File available at', downloadURL);
+        });
+      });
+};
+$("#file").on("change",function(event){
+    selectedFile = event.target.files[0];   
+});
---
+++ UI/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function CreateWindow(){
+    win = new BrowserWindow({width:1920, height:1080, icon:__dirname+'/icon.png'});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    console.log("WINDOWS CREATED");
+    
+    win.webContents.openDevTools();
+    win.on('closed',() => {
+        win=null;
+    });
+}
+app.on('ready', CreateWindow);
+
+app.on('window-all-closed',() => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
